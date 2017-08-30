9c39f8b6ed1ea4fd534ba1240f3c1131afdd2d6d
wzj <wzj@ffe548d7-c07d-3545-b42d-f1e43fec7791>
2017-08-30 08:33:52 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xyt@97 ffe548d7-c07d-3545-b42d-f1e43fec7791

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Hifone.
  *
- * 
+ *
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -28,6 +28,7 @@ class Blog extends Model
      */
     protected $fillable = [
         'user_id',
+		'cat_id',
         'title',
         'space_id',
         'body',
