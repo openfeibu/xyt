5616de1c3adf6e00aa597baac9341fa7477846af
wzj <wzj@ffe548d7-c07d-3545-b42d-f1e43fec7791>
2017-09-15 10:12:59 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xyt@109 ffe548d7-c07d-3545-b42d-f1e43fec7791

--- resources/views/albums/album.blade.php
+++ resources/views/albums/album.blade.php
@@ -4,9 +4,11 @@
 <link href="{{ asset('/build/dist/css/album.css') }}" type="text/css" rel="stylesheet" />
 <div class="clear"></div>
     <div class="album">
+        <form name="delPhotos" action="{{route('album.delAlbums')}}" method="post">
         <div class="album_nav">
             <div style="height: 20px;"></div>
             @include('albums.album_nav')
+            <div ><input name="submit" type="submit" value="删除" class="del_btn"/></div>
             <div class="clear"></div>
         </div>
         <div class="clear"></div>
@@ -18,12 +20,13 @@
                         <a href="{{ route('album.album_photos',['album_id'=>$album->id]) }}"><img src="{{$album->image}}" alt="" class="album_centent_photo_img" /></a>
                     </dd>
                     <dd class="album_centent_photo_dd2" style="text-align: center;">
-                        {{$album->name}}
+                        <input type="checkbox" name="ids[]" value="{{$album->id}}" >{{$album->name}}
                     </dd>
                 </dl>
             </div>
             @endforeach
         </div>
+        </form>
     </div>
 
     {!! with(new \Hifone\Foundations\Pagination\CustomerPresenter($albums))->render(); !!}
--- resources/views/albums/album_photo.blade.php
+++ resources/views/albums/album_photo.blade.php
@@ -4,11 +4,15 @@
 <link href="{{ asset('/build/dist/css/album.css') }}" type="text/css" rel="stylesheet" />
 <div class="clear"></div>
     <div class="album">
+        <form name="delPhotos" action="{{route('album.delPhotos')}}" method="post">
         <div class="album_nav">
             <div style="height: 20px;"></div>
             @include('albums.album_nav')
+            <div ><input name="submit" type="submit" value="删除" class="del_btn"/></div>
             <div class="clear"></div>
         </div>
+        <div class="clear"></div>
+
         <div class="clear"></div>
         <div class="album_centent">
 	        @foreach($album_photos as $album_photo)
@@ -17,10 +21,15 @@
                     <dd>
                         <a href="{{ route('album.show',$album_photo->id) }}" ><img src="{{ $album_photo->image }}" alt="" class="album_centent_photo_img" /></a>
                     </dd>
+
+                    <dd>
+                        <p style="text-align:center"><input type="checkbox" name="ids[]" value="{{$album_photo->id}}" ></p>
+                    </dd>
                 </dl>
             </div>
             @endforeach
         </div>
+        </form>
     </div>
 
     {!! with(new \Hifone\Foundations\Pagination\CustomerPresenter($album_photos))->render(); !!}
