4d8a1422591bea55829dd4c8d7cb6ff1d98753d4
wzj <wzj@ffe548d7-c07d-3545-b42d-f1e43fec7791>
2017-10-03 12:13:27 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xyt@132 ffe548d7-c07d-3545-b42d-f1e43fec7791

--- config/alisms.php
+++ config/alisms.php
@@ -1,9 +1,14 @@
 <?php
 return [
+    /*
     'KEY' =>env('ALISMS_KEY','23391567'),
     'SECRETKEY'=>env('ALISMS_SECRETKEY','7972d2dc53b68b63c346df0de5df0b31'),
 
     'sign' => '校汇',
     'changMobile' => 'SMS_14900048',
-
+    */
+    'accessKeyId' => 'LTAIDuhnq6K1Kc20',
+    'accessKeySecret' => 'e4ffFWI4SRTphWkTyXzpPQgHGe9rAN',
+    'signName' => '象牙塔',
+    'changMobile' => 'SMS_101090028',
 ];
